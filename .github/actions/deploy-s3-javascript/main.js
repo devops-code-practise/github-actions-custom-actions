741c393f961209a8fc43a1b1257fff585a9e1f39
Mikhiei Ulianov <itituser09@gmail.com>
2026-06-20 12:41:47 +0200

    added custom deploy job

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,19 @@
-const core = requite("@actions/core");
-const github = requite("@actions/github");
-const exec = requite("@actions/exec");
+const core = require("@actions/core");
+// const github = require("@actions/github");
+const exec = require("@actions/exec");
 
 function run() {
+  //   allows to get provided inputs
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // upload files
+  //   the CLI is pre-installed on ubuntu images
+  const s3Uri = `s3://${bucket}`; // syncing with an s3 bucket
+  // CLI will automatically look for the env varaibles
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
   core.notice("Custom JS Actions");
 }
 
